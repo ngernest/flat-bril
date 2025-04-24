a020a6eb6a95f1fdcab8f53deaac41f60717011c
Ernest Ng <eyn5@cornell.edu>
2025-04-24 13:26:36 -0400

    Some very basic JSON infrastructure

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # A Flattened Representation for Bril
 
+Run `cargo build` to compile. 
+Example usage:
+```bash
+$ bril2json < test/tiny.bril | cargo run
+```
+
 Repo structure:
 - [`bril-rs`](./bril-rs/): existing JSON to Bril infra
 - [`brilirs`](./brilirs/): existing Bril interpreter 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[allow(unused_imports)]
+use serde_json::{json, Error, Value};
+use std::{io, io::Read};
+
+#[allow(dead_code)]
+/// Flattened type for Bril instructions
+struct Inst {
+    op: usize,
+    dest: usize,
+    ty: usize,
+    args: (usize, usize),
+    label: (usize, usize),
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Enable stack backtrace for debugging
+    unsafe {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("Unable to read from stdin");
+    let json: Value =
+        serde_json::from_str(&buffer).expect("Unable to parse malformed JSON");
+    println!("{}", json);
 }
