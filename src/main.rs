d1063f31852dd11fc73e957506099f1005253c91
Ernest Ng <eyn5@cornell.edu>
2025-04-24 13:57:12 -0400

    Print out each instruction from the JSON

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,24 @@ struct Inst {
 }
 
 fn main() {
-    // Enable stack backtrace for debugging
-    unsafe {
-        std::env::set_var("RUST_BACKTRACE", "1");
-    }
-
     let mut buffer = String::new();
     io::stdin()
         .read_to_string(&mut buffer)
         .expect("Unable to read from stdin");
-    let json: Value =
-        serde_json::from_str(&buffer).expect("Unable to parse malformed JSON");
-    println!("{}", json);
+    let json: Value = serde_json::from_str(&buffer).expect("Unable to parse malformed JSON");
+    let functions = json["functions"]
+        .as_array()
+        .expect("Expected `functions` to be a JSON array");
+    for func in functions {
+        let name = func["name"]
+            .as_str()
+            .expect("Expected `name` to be a string");
+        println!("{}", name);
+        let instrs = func["instrs"]
+            .as_array()
+            .expect("Expected `instrs` to be a JSON array");
+        for instr in instrs {
+            println!("{}", instr);
+        }
+    }
 }
