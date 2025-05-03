ab70b464406eb5612ccc7e222a07a1f97e3af7d9
Ernest Ng <eyn5@cornell.edu>
2025-05-03 18:42:19 -0400

    add todo

--- src/unflatten.rs
+++ src/unflatten.rs
@@ -3,6 +3,10 @@ use crate::types::*;
 #[allow(unused_imports)]
 use serde_json::json;
 
+// TODO: figure out how to handle labels when unflattening since they're
+// not currently represented in the `InstrStore`
+// (labels are just printed directly to stdout when we flatten)
+
 /// Takes an `InstrStore` (flattened instrs + arrays storing args/dests etc.)
 /// corresponding to a Bril function and returns its JSON representation
 #[allow(dead_code)]
@@ -88,7 +92,6 @@ pub fn unflatten_instrs<'a>(instr_store: &'a InstrStore) -> serde_json::Value {
               "labels": labels,
               "funcs": funcs
             });
-            todo!("figure out how to implement json for effect op")
         }
     }
 
