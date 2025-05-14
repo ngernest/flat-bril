2541c5f872d451285119313ae03e27a361db11ce
Ernest Ng <eyn5@cornell.edu>
2025-05-14 18:42:56 -0400

    Update JSON roundtrip benchmarking script

--- bench.sh
+++ bench.sh
@@ -3,11 +3,19 @@
 # Create a hyperfine command with all benchmarks
 CMD="hyperfine -w3 --shell=none --show-output --export-markdown json_roundtrip_bench.md"
 
+benchmarks=("bitshift" "call" "catalan" "euclid" "main-args" "montgomery" "nop" "perfect" "reverse" "rot13")
+
 for file in test/*.json; do
   # Extract just the benchmark name (without path or extension)
   name=$(basename "$file" .json)
-  # Add to hyperfine command
-  CMD+=" --command-name \"$name\" \"./target/release/flat-bril --json --filename $file\""
+
+  for benchmark in "${benchmarks[@]}"; do
+    if [ "$name" = "$benchmark" ]; then
+    # Add to hyperfine command
+    CMD+=" --command-name \"$name\" \"./target/release/flat-bril --json --filename $file\""
+    break 
+    fi 
+  done
 done
 
 # Execute the command
